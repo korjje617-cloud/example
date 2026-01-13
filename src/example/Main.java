3bfa0372950201c6393f9d037e81ec22bf9a070e
korjje617 <korjje617@gmail.com>
2026-01-14 01:50:19 +0900

    개발환경세팅

--- src/example/Main.java
+++ src/example/Main.java
@@ -1,7 +1,7 @@
 package example;
 
 public class Main {
-public static void main(String[] args) {
-	System.out.println("진짜너무힘들다설치만한시간걸렸어이게맞아?");
-}
+	public static void main(String[] args) {
+		System.out.println("진짜너무힘들다설치만한시간걸렸어이게맞아?");
+	}
 }
